73432a1dc27f588d477f68b3c0c0a1fac7972250
Diyorbek <D1Y0RBEKORIFJONOV>
2024-04-17 18:49:06 +0500

    Helo World

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"github.com/D1Y0RBEKORIFJONOV/repo_test_1/greeting"
 )
 
-
 func main() {
 	fmt.Print("Hello World!")
-	fmt.Print(greeting.Greet(),greeting.InputName())
+	fmt.Print(greeting.Greet(), greeting.InputName())
+
 }
